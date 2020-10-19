ecc41a62c33c600104462bff4b00e0de09a43677
Ryuta Kamizono <kamipo@gmail.com>
2020-10-19 21:57:43 +0900

    Avoid extra string allocation by `# frozen_string_literal: true`
    In the codebase, there are some extra string allocation in runtime,
    especially `"valid_from"`, `"valid_to"`, and `"bitemporal_id"`.
    
    https://github.com/kufu/activerecord-bitemporal/blob/f56b4293a774d9f5a0a49114c561b39175509190/lib/activerecord-bitemporal/bitemporal.rb#L122-L126
    https://github.com/kufu/activerecord-bitemporal/blob/f56b4293a774d9f5a0a49114c561b39175509190/lib/activerecord-bitemporal.rb#L38-L40
    
    `# frozen_string_literal: true` magic comment avoids that extra string
    allocation, so I've (automatically) applied that magic comment by
    `rubocop -A` by the following `.rubocop.yml`.
    
    ```
    AllCops:
      DisabledByDefault: true
    
    Layout/EmptyLineAfterMagicComment:
      Enabled: true
    
    Style/FrozenStringLiteralComment:
      Enabled: true
      EnforcedStyle: always
    ```

--- lib/activerecord-bitemporal/patches.rb
+++ lib/activerecord-bitemporal/patches.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative "./bitemporal.rb"
 
 module ActiveRecord::Bitemporal
--- lib/activerecord-bitemporal/version.rb
+++ lib/activerecord-bitemporal/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ActiveRecord
   module Bitemporal
     VERSION = "0.1.0"
