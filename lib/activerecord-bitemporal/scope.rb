524912e934869f2fbf5cd9cdb25f61ebc7a0cbaf
manga_osyo <manga.osyo@gmail.com>
2021-02-10 16:27:24 +0900

    In Rails 6.1 and above, we also need to consider `Arel::Nodes::Or` and `Arel::Nodes::And`

--- lib/activerecord-bitemporal/scope.rb
+++ lib/activerecord-bitemporal/scope.rb
@@ -50,6 +50,11 @@ module ActiveRecord::Bitemporal
                 case node
                 when Arel::Nodes::LessThan, Arel::Nodes::LessThanOrEqual, Arel::Nodes::GreaterThan, Arel::Nodes::GreaterThanOrEqual
                   y << node if node && node.left.respond_to?(:relation)
+                when Arel::Nodes::Or, Arel::Nodes::And
+                  if Gem::Version.new("6.1.0") <= ActiveRecord.version
+                    each_operatable_node(node.left) { |node| y << node }
+                    each_operatable_node(node.right) { |node| y << node }
+                  end
                 when Arel::Nodes::Grouping
                   each_operatable_node(node.expr) { |node| y << node }
                 end
